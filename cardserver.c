a63a92edb1cdbad2e1b86dadbcc003ff25e3fb58
Jake Bladt <jake@jakebladt.com>
2014-04-12 22:29:24 -0400

    non-standard port

--- cardserver.c
+++ cardserver.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
-	int port = (argc <= 1) ? 80: atoi(argv[1]);
+	int port = (argc <= 1) ? 8014: atoi(argv[1]);
 	printf("listening on port %i", port);
 	return 0;
 }
