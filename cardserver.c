fa7b87f3a324c8cf50541c66398205ea0d979f09
Jake Bladt <jake@jakebladt.com>
2014-04-12 22:48:43 -0400

    boy is my C rusty.

--- cardserver.c
+++ cardserver.c
@@ -1,8 +1,17 @@
+#include <Objbase.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
+
+GUID getNewSleeve(int deckCount) {
+	GUID guidReference;
+	HRESULT hCreate = CoCreateGuid(&guidReference);
+	return guidReference;
+}
 
 int main(int argc, char **argv) {
 	int port = (argc <= 1) ? 8014: atoi(argv[1]);
 	printf("listening on port %i", port);
+	printf("deck ID is %s", getNewSleeve(1));
 	return 0;
 }
